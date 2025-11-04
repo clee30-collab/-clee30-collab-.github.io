d8ea525b0f9320299032c895b47e8e46bd52c053
clee30-collab <clee30@risd.edu>
2025-11-04 04:39:32 -0500

    finished bedroom and attic

--- project1/js/main.js
+++ project1/js/main.js
@@ -1,3 +1,28 @@
-// Main JavaScript for house.html
-// Room images appear on hover via CSS, no JavaScript needed for basic functionality
+// Select all images that have a data-sound attribute
+const roomImages = document.querySelectorAll('[data-sound]');
 
+roomImages.forEach(img => {
+  let audio;
+
+  img.addEventListener('mouseenter', () => {
+    const soundSrc = img.dataset.sound;
+    if (!soundSrc) return;
+
+    // Stop previous sound if already playing
+    if (audio) {
+      audio.pause();
+      audio.currentTime = 0;
+    }
+
+    // Play new sound
+    audio = new Audio(soundSrc);
+    audio.play();
+  });
+
+  img.addEventListener('mouseleave', () => {
+    if (audio) {
+      audio.pause();
+      audio.currentTime = 0;
+    }
+  });
+});
